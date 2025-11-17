2d0d2ad76ae53603c8a1778828e06793704dd33a
Manish7380 <mj3246353@gmail.com>
2025-11-18 01:49:32 +0530

    shift mongodb to online server

--- nodejs/server5.js
+++ nodejs/server5.js
@@ -4,9 +4,11 @@ const express = require('express');
 
 const app = express();
 const db = require('./db');
+require('dotenv').config(); // Load environment variables from .env file
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = precess.env.PORT || 3000;
 // Import models
 
 const MenuItem = require('./models/MenuItem');
@@ -29,6 +31,6 @@ const menuItemRoutes = require('./routes/menuRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('✅ Server is running at http://localhost:3000/');
 })
